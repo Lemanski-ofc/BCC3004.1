85186a81239cf4a1f2a36b47212eb739c4a577e0
Lemanski <112637318+Lemanski-ofc@users.noreply.github.com>
2024-07-25 13:24:08 -0300

    Update factory_methody.cpp

--- factory_methody.cpp
+++ factory_methody.cpp
@@ -1,16 +1,4 @@
-/*Descrição:
-O padrão Factory Method define uma interface para criar um objeto, mas permite que as subclasses decidam qual classe instanciar. 
-Ele encapsula a criação de objetos e fornece uma maneira de delegar a lógica de instanciação para subclasses.
-
-Problema:
-Quando temos uma classe que não sabe de antemão qual objeto precisa criar, ou quando queremos delegar a responsabilidade de criação de 
-objetos para subclasses.
-
-Solução:
-O Factory Method define um método que deve ser implementado pelas subclasses para criar um objeto específico. Isso permite que a lógica
-de criação de objetos seja desacoplada do código cliente que usa esses objetos.
-
-Diagrama:
+/*Diagrama:
                    +------------------+
                   |     Creator      |
                   +------------------+
